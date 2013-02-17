c3aea16c9cc225a48c4eb510b92cd6757196d6d4
Daniel Wonisch <daniel.wonisch@gmail.com>
2013-02-17 23:47:08 +0100

    Added ToStringOrEmpty extension method

---
+++ CommonExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensions
+{
+    public static class CommonExtensions
+    {
+        public static string ToStringOrEmpty(this object value) {
+            return value + string.Empty;
+        }
+    }
+}
